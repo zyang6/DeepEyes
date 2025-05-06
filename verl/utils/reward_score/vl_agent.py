3a630f6e09df4bf7edb7256c0e445ff191a1281c
fengyuan <chenxiao2@xiaohongshu.com>
2025-05-06 16:20:22 +0000

    update vl verify

--- verl/utils/reward_score/vl_agent.py
+++ verl/utils/reward_score/vl_agent.py
@@ -1,12 +1,14 @@
 from openai import OpenAI
 import requests
 import random
+import re
+
+from math_verify import parse, verify
 
 openai_api_key = "EMPTY"
-
 openai_api_base_list = [
-    "http://10.39.11.28:10000/v1",
-    "http://10.39.11.27:10000/v1",
+    # "http://172.30.52.123:8000/v1", # 72b-it
+    "http://10.39.3.123:18901/v1",
 ]
 
 client_list = []
@@ -21,10 +23,6 @@ for client in client_list:
     response = requests.get(f"{api_base}/models")
     models = response.json()
     model_name_list.append(models['data'][0]['id'])
-
-
-
-
 
 
 
@@ -88,6 +86,70 @@ Judgement: 0
 
     return [example_1, example_2, example_3, example_4, example_5, example_6, example_7]
 
+COMMON_VERIFY_PROMPT = """# CONTEXT #
+I am a teacher, and I have some high-level reasoning problems. I am tasked with evaluating the correctness of a student's answer. 
+Below, I am provided with a problem and a reference answer. Additionally, a student's answer is provided. My job is to assess whether the student's answer captures the same meaning as the reference answer, even when expressed with different wording or format.
+
+# OBJECTIVE #
+I need you to judge whether the student's answer is correct given the ground truth answer.
+
+Your tasks include:
+1. Identify Semantic Equivalence: Carefully examine the expression in both answers. Confirm whether the semantic meaning of student's final answer is equivalent to the reference answer, even when expressed with different wording or format.
+
+# TONE #
+Professional, scientific.
+
+# RESPONSE: MARKDOWN REPORT #
+## Equivalence Judgement
+[Whether the student's answer share the same meaning with the reference answer. (TRUE or FALSE)]
+
+# ATTENTION #
+ - The reference answer is ALWAYS correct. You should carefully judge whether the student gives the same answer as reference answer.
+ - The Equivalence Judgement is only TRUE or FALSE. The answer is FALSE even if the student's final answer almost correct with a minor mistakes.
+ - Don't give extra explanation.
+
+**Question**:
+{query}
+
+**Reference Answer**
+{gold_ans}
+
+## Student Final Answer
+{pred_ans}"""
+
+
+MATH_VERIFY_PROMPT = """# CONTEXT #
+I am a teacher, and I have some high-level math problems. I am tasked with evaluating the correctness of a student's answer. 
+Below, I am provided with a problem and a reference answer. Additionally, a student's answer is provided. My job is to assess whether the student's answer captures the same meaning as the reference answer, even when expressed with different wording or format.
+
+# OBJECTIVE #
+I need you to judge whether the student's answer is correct given the ground truth answer.
+
+Your tasks include:
+1. Identify Mathematical or Notational Equivalence: Pay special attention to any LaTeX expressions in both answers. Confirm that the mathematical relationships, variables, and operations conveyed are equivalent.
+
+# TONE #
+Professional, scientific.
+
+# RESPONSE: MARKDOWN REPORT #
+## Equivalence Judgement
+[Whether the student's answer share the same meaning with the reference answer. (TRUE or FALSE)]
+
+# ATTENTION #
+ - The reference answer is ALWAYS correct. You should carefully judge whether the student gives the same answer as reference answer.
+ - The Equivalence Judgement is only TRUE or FALSE. The answer is FALSE even if the student's final answer almost correct with a minor mistakes.
+ - Don't give extra explanation.
+
+**Question**:
+{query}
+
+**Reference Answer**
+{gold_ans}
+
+## Student Final Answer
+{pred_ans}"""
+
+
 def get_prompt(predict_str, ground_truth, question):
     examples = get_gpt4_score_ICE()
     chat_template = get_chat_template()
@@ -105,9 +167,27 @@ Judgement:"""
     return full_prompt
 
 
+def extract_answer(text):
+    """
+    从给定的文本中提取<answer></answer>标签内部的内容。
+    
+    参数:
+        text (str): 包含<answer>标签的文本
+        
+    返回:
+        str or None: 标签内部的内容，如果未找到则返回None。
+    """
+    # 使用非贪婪模式匹配<answer>和</answer>之间的内容
+    pattern = r'<answer>(.*?)</answer>'
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        return match.group(1).strip()
+    return None
+
+
 def compute_score(predict_str: str, ground_truth: str, extra_info=None) -> float:
     is_format_error = False
-    predict_str = "<think>" + predict_str
+    # predict_str = "<think>" + predict_str
     count_think_1 = predict_str.count("<think>")
     count_think_2 = predict_str.count("</think>")
     if count_think_1 != count_think_2:
@@ -124,7 +204,16 @@ def compute_score(predict_str: str, ground_truth: str, extra_info=None) -> float
     if count_answer_1 != count_answer_2:
         is_format_error = True
 
-    answer_text = predict_no_think.split("<answer>")[-1].split("</answer>")[0].strip()
+    answer_text = predict_str.split("<answer>")[-1].split("</answer>")[0].strip()
+
+    # pattern = re.compile(r'<\|im_start\|>assistant(.*?)$', re.DOTALL)  # 匹配最后一个 target 后的所有内容
+    # match = pattern.search(predict_str)
+    # if match:
+    #     answer_text = match.group(1).strip()
+    #     print(f'DEBUG{answer_text=}')
+    # else:
+    #     answer_text = ""
+
     question_text = extra_info['question']
     full_prompt = get_prompt(answer_text, ground_truth, question_text)
 
@@ -142,6 +231,7 @@ def compute_score(predict_str: str, ground_truth: str, extra_info=None) -> float
         temperature=0.3,
     )
     response = chat_response.choices[0].message.content.strip()
+    # print(response)
     if 'Judgement:' in response:
         response = response.split('Judgement:')[-1].strip()
         if '1' in response:
@@ -168,7 +258,158 @@ def compute_score(predict_str: str, ground_truth: str, extra_info=None) -> float
     tool_reward_base = 1.0 if count_vision_1 > 0 else 0.0
     tool_reward = 1.0 if count_vision_1 > 0 and acc_reward > 0.5 else 0.0
     format_reward = -1.0 if is_format_error else 0.0
-    return 1.0 * acc_reward + 0.2 * format_reward + 1.0 * tool_reward + 0.2 * tool_reward_base
+    # reward 1
+    return 0.8 * acc_reward + 0.2 * format_reward + 0.4 * tool_reward_base  
+    # reward 2 
+    # return 1.0 * acc_reward + 0.2 * format_reward + 1.0 * tool_reward + 0.2 * tool_reward_base
+    # reward 3
+    # tool_reward_alpha = 1.2 if count_vision_1 > 0 else 0.0
+    # return 1.0 * acc_reward * tool_reward_alpha + 0.2 * format_reward
+    # reward 4
+    # extra_reward = tool_reward_base * (count_vision_1 - 1) * (1 - acc_reward)
+    # return  0.8 * acc_reward + 0.2 * format_reward + 0.4 * tool_reward_base  + 0.2 * extra_reward
+
+
+
+def compute_common_reasoning(predict_str: str, ground_truth: str, extra_info=None) -> float:
+    is_format_error = False
+    # predict_str = "<think>" + predict_str
+    count_think_1 = predict_str.count("<think>")
+    count_think_2 = predict_str.count("</think>")
+    if count_think_1 != count_think_2:
+        is_format_error = True
+
+    count_vision_1 = predict_str.count("<|vision_start|><|image_pad|>")
+    count_vision_2 = predict_str.count("<|image_pad|><|vision_end|>")
+    if count_vision_1 != count_vision_2:
+        is_format_error = True
+
+    predict_no_think = predict_str.split('</think>')[-1].strip()
+    count_answer_1 = predict_no_think.count("<answer>")
+    count_answer_2 = predict_no_think.count("</answer>")
+    if count_answer_1 != count_answer_2:
+        is_format_error = True
+
+    answer_text = extract_answer(predict_no_think) # predict_no_think.split("<answer>")[-1].split("</answer>")[0].strip()
+    if not answer_text:
+        acc_reward = 0.0
+        is_format_error = True
+    elif len(answer_text) >= 300:
+        acc_reward = 0.0
+        is_format_error = True
+    else:
+        if len(answer_text.split(',')) != 2:
+            is_format_error = True
+
+        question_text = extra_info['question']
+        client_idx = random.randint(0, len(client_list) - 1)
+        client = client_list[client_idx]
+        model_name = model_name_list[client_idx]
+        full_prompt = COMMON_VERIFY_PROMPT.format(
+            query="In which country and within which first-level administrative region of that country was this picture taken?",
+            gold_ans=ground_truth,
+            pred_ans=answer_text,
+        )
+
+        acc_reward = 0.0
+        for ix in range(8):
+            chat_response = client.chat.completions.create(
+                model=model_name,
+                messages=[
+                    {"role": "user", "content": full_prompt},
+                ],
+                seed = random.randint(0, 1000000),
+                temperature=0.5,
+            )
+            response = chat_response.choices[0].message.content.strip()
+            judgement = response.split('## Equivalence Judgement')[-1].lower()
+            if 'true' in judgement and 'false' not in judgement:
+                acc_reward = 1.0
+                break
+            elif 'false' in judgement and 'true' not in judgement:
+                acc_reward = 0.0
+                break
+            else:
+                print(f' [ERROR] judgement format invalid: {judgement}')
+                continue
+
+    tool_reward_base = 1.0 if count_vision_1 > 0 else 0.0
+    tool_reward = 1.0 if count_vision_1 > 0 and acc_reward > 0.5 else 0.0
+    format_reward = -1.0 if is_format_error else 0.0
+    return 0.8 * acc_reward + 0.2 * format_reward + 0.4 * tool_reward_base + 0.4 * tool_reward
+
+
+def rule_math_verify(ground_truth, model_answer):
+    gold = parse(ground_truth)
+    answer = parse(model_answer)
+    return verify(gold, answer)
+
+
+def generative_verify(query, ground_truth, model_answer):
+    client_idx = random.randint(0, len(client_list) - 1)
+    client = client_list[client_idx]
+    model_name = model_name_list[client_idx]
+
+    full_prompt = MATH_VERIFY_PROMPT.format(
+        query=query,
+        gold_ans=ground_truth,
+        pred_ans=model_answer,
+    )
+
+    response = ""
+    for it in range(8):
+        try:
+            chat_response = client.chat.completions.create(
+                model=model_name,
+                messages=[
+                    {"role": "user", "content": full_prompt},
+                ],
+                seed = random.randint(0, 1000000),
+                temperature=0.0,
+            )
+            response = chat_response.choices[0].message.content.strip()
+            break
+        except Exception as e:
+            print(f' [ERROR math] generative_verify error: {e}')
+            continue
+    
+    judgement = response.split('## Equivalence Judgement')[-1].lower()
+    if 'true' in judgement and 'false' not in judgement:
+        return True
+    elif 'false' in judgement and 'true' not in judgement:
+        return False
+    else:
+        print(f' [ERROR math] verify bug output: ')
+
+
+def compute_score_math(predict_str: str, ground_truth: str, extra_info=None) -> float:
+    is_format_error = False
+    # predict_str = "<think>" + predict_str
+    count_think_1 = predict_str.count("<think>")
+    count_think_2 = predict_str.count("</think>")
+    if count_think_1 != count_think_2:
+        is_format_error = True
+
+    model_answer = ""
+    predict_no_think = predict_str.split('</think>')[-1].strip()
+    answer_pattern = r'\\boxed{([^}]+)}'
+    answer_list = re.findall(answer_pattern, predict_no_think, flags=re.DOTALL)
+    if len(answer_list) == 0:
+        acc_reward = 0.0
+        is_format_error = True
+    else:
+        if len(answer_list) > 1:
+            is_format_error = True
+
+        model_answer = answer_list[-1]
+        if rule_math_verify(ground_truth, model_answer):
+            acc_reward = 1.0
+        else:
+            acc_reward = 1.0 if generative_verify(extra_info['question'], ground_truth, model_answer) else 0.0
+    
+    format_reward = -1.0 if is_format_error else 0.0
+    print(f' [DEBUG] query={extra_info["question"]}, {ground_truth=}, {model_answer=}, {acc_reward=}, {format_reward=}')
+    return 1.2 * acc_reward + 0.4 * format_reward
 
 
 if __name__ == '__main__':
